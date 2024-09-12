a54b8e70ea935c8cdb4568908598ec8abb013786
Babu Reddy <babureddy1969@gmail.com>
2024-09-12 13:06:24 +0530

    Add files via upload
    Added arguments to get data and process data

--- predict.py
+++ predict.py
@@ -4,40 +4,65 @@ from datetime import datetime
 from datetime import date
 from nsepython import *
 import os.path
-
 import sys
-
-# total arguments
+stock_list = []
+def getStockList():
+  with open('EQUITY_L.csv', mode='r',encoding='utf-8-sig') as infile: 
+    next(infile) # skip header
+    reader = csv.reader(infile)
+    for row in reader:
+        stock_list.append(row[0])
+    return stock_list
+# print(stock_list)
+# exit(0)
+def getData():
+    start_date = "10-09-2023"
+    end_date = "10-09-2024"
+    for symbol in stock_list:
+        f = symbol + '.csv'
+        if os.path.isfile(f) == False:
+            print(symbol, ' File does not exist.')
+            equity_history(symbol,'EQ',start_date, end_date).to_csv(f)
+def process():
+    format_string = "%Y-%m-%d"
+    mydict = {}
+    for f in stock_list:
+        if os.path.isfile(f+'-result.csv') == True: continue
+        print("Processing " + f)
+        with open(f+".csv", mode='r',encoding='utf-8-sig') as infile:
+            next(infile) # skip header
+            reader = csv.reader(infile)
+            for rows in reader:
+                price = rows[5]
+                if '-' in rows[5]:price = rows[7]
+                mydict[datetime.datetime.strptime(rows[20][0:10], format_string)] = price
+            keys = sorted(mydict.keys())
+            sorted_dict= {}
+            for k in keys:
+                sorted_dict[k] = mydict.get(k)
+        val = 0.0
+        s = ""
+        for k,v in sorted_dict.items():
+            v = float(v.replace(',',''))
+            if v > val: 
+                # print(k,v, int(v-val))
+                val = v
+                s += k.strftime("%d-%m-%Y") + " " + str(v) + " " + str(int(v - val)) + '\n'
+        with open(f+'-result.csv',"w") as fil:
+            fil.write(s)
 n = len(sys.argv)
-symbol = 'SBIN'
-# print("No of arguments:",n, sys.argv)
+symbol = ''
 if n > 1 :
     symbol = sys.argv[1]
     print(symbol, end = " ")
-format_string = "%Y-%m-%d"
-mydict = {}
-# f = 'BAJFINANCE-EQ-08-09-2023-to-08-09-2024.csv'
-# f = 'Quote-Equity-WIPRO-EQ-09-09-2023-to-09-09-2024.csv'
-# f = 'Quote-Equity-JIOFIN-EQ-09-09-2023-to-09-09-2024.csv'
-# f = 'Quote-Equity-COALINDIA-EQ-09-09-2023-to-09-09-2024.csv'
-# f = 'Quote-Equity-SBIN-EQ-09-09-2023-to-09-09-2024.csv'
-f = symbol + '.csv'
-start_date = "09-09-2023"
-end_date = "09-09-2024"
-if os.path.isfile(f) == False:
-    print('File does not exist.')
-    equity_history(symbol,'EQ',start_date, end_date).to_csv(f)
-with open(f, mode='r',encoding='utf-8-sig') as infile:
-    next(infile) # skip header
-    reader = csv.reader(infile)
-    mydict = dict((datetime.datetime.strptime(rows[20][0:10], format_string),rows[5]) for rows in reader)
-    keys = sorted(mydict.keys())
-    sorted_dict= {}
-    for k in keys:
-        sorted_dict[k] = mydict.get(k)
-val = 0.0
-for k,v in sorted_dict.items():
-    v = float(v.replace(',',''))
-    if v > val: 
-        val = v
-        print(k,v)
+    if symbol == "ALL": 
+        getStockList()
+    else :
+        stock_list = [symbol]
+        
+    if n == 3 and sys.argv[2] == 'PROCESS': process()
+    if n == 3 and sys.argv[2] == 'GETDATA': getData()
+else:
+    getStockList()
+    getData()
+    process()
